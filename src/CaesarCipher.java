ded84f503d96e07e5bdda59914eafa563d8ec69a
Timur Mannapov <bla.blacarr@yandex.ru>
2023-12-26 21:34:47 +0300

    Create class CaesarCipher

---
+++ src/CaesarCipher.java
@@ -0,0 +1,9 @@
+public class CaesarCipher {
+    private static final String alphabet = "abc";
+    public String encrypt(String message, int key) {
+        return "";
+    }
+    public String decrypt(String message, int key) {
+        return "";
+    }
+}
