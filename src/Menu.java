0e6db66610d72f44deb6de2cbaadb34aa96bf58d
Timur Mannapov <bla.blacarr@yandex.ru>
2023-12-26 21:07:51 +0300

    Create menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Menu {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("""
+                    Выберете действие введя его номер:\s
+                    1. Зашифровать текст в файле с помощью ключа.\s
+                    2. Расшифровать текст в файле с помощью ключа.\s
+                    3. Подобрать ключ используя brute force.\s
+                    4. Расшифровать текст методом синтаксического анализа. \s
+                    5. Выйти из программы.""");
+            String answer = scanner.nextLine();
+            switch (answer) {
+                case "1" -> System.out.println("Зашифровать текст в файле с помощью ключа.");
+                case "2" -> System.out.println();
+                case "3" -> System.out.println();
+                case "4" -> System.out.println();
+                case "5" -> {
+                    return;
+                }
+            }
+
+
+        }
     }
 }
