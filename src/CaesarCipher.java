fa963f616aaf9b8bacb3e72eb97bdd079d28efd5
Timur Mannapov <bla.blacarr@yandex.ru>
2023-12-30 13:58:14 +0300

    In class CaesarCipher add punctuation marks Create ArrayList<String> encrypt

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -3,14 +3,27 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class CaesarCipher {
-    private static final String alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-    ArrayList<String> alphaBet  = new ArrayList<>(Arrays.asList(alphabet));
+    private static final String alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,””:-!? ";
+    ArrayList<String> alphaBet = new ArrayList<>(Arrays.asList(alphabet));
 
-    public String encrypt(String message, int key) {
 
-        return "";
+    public ArrayList<String> encrypt(String message, int key) {
+        ArrayList<String> enCrypt = new ArrayList<>();
+        enCrypt.add(message);
+        for (int i = 0; i < enCrypt.size(); i++) {
+            int index = 0;
+            if (alphabet.contains(enCrypt.get(i))) {
+                index = alphabet.indexOf(enCrypt.get(i));
+                enCrypt.set(i, alphaBet.get(index + key));
+            } else {
+                return enCrypt;
+            }
+        }
+        return null;
     }
-    public String decrypt(String message, int key) {
-        return "";
+        public String decrypt (String message,int key){
+            return "";
+        }
     }
-}
+
+
