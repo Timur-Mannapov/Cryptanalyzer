af9e6f16169098f4a0b2884995f6df368e3f47cd
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-02 16:00:48 +0300

    Changed class BruteForce. Added functional. Added  - while !decryptText

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -2,20 +2,24 @@ import java.util.Scanner;
 
 public class BruteForce {
     public static void main(String[] args) {
-        System.out.println("Введите текст, который нужно расшифровать");
         Scanner scanner = new Scanner(System.in);
         CaesarCipher caesarCipher = new CaesarCipher();
+        System.out.println("Введите текст, который нужно расшифровать");
         String string = scanner.nextLine();
+        int key = 1;
+        String decryptText = caesarCipher.decrypt(string, key);
+        System.out.println(decryptText);
+        System.out.println("Текст расшифрован?");
+        System.out.println("Да/Нет");
         String answer = scanner.nextLine();
-
-            for (int i = 1; i < 41; i++) {
-                int key = i;
-                caesarCipher.decrypt(string, key);
+        while (answer.equals("Нет")) {
+            while (!(decryptText.equals(", ")) && !(decryptText.equals(". "))) {
+                key += 1;
+                System.out.println(decryptText);
                 System.out.println("Текст расшифрован?");
-                System.out.println("Yes/No");
+                System.out.println("Да/Нет");
+                answer = scanner.nextLine();
             }
         }
-
-
-        }
     }
+}
