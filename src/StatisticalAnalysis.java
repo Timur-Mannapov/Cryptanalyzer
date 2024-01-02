1a81f42beccf234f7bd2065ec5521ed1037e28e3
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-02 16:16:21 +0300

    Changed class StatisticalAnalysis.

--- src/StatisticalAnalysis.java
+++ src/StatisticalAnalysis.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
+
 public class StatisticalAnalysis {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CaesarCipher caesarCipher = new CaesarCipher();
+        System.out.println("Введите текст, который нужно расшифровать");
+        String string = scanner.nextLine();
+        int key = 1;
+        String decryptText = caesarCipher.decrypt(string, key);
+        String anotherText = scanner.nextLine();
+    }
 }
