f8e2e07b1713a53b2688e43b8da3d395f3045d15
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-02 15:16:01 +0300

    Changed class BruteForce.

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class BruteForce {
-}
+    public static void main(String[] args) {
+        System.out.println("Введите текст, который нужно расшифровать");
+        Scanner scanner = new Scanner(System.in);
+        CaesarCipher caesarCipher = new CaesarCipher();
+        String string = scanner.nextLine();
+        String answer = scanner.nextLine();
+
+            for (int i = 1; i < 41; i++) {
+                int key = i;
+                caesarCipher.decrypt(string, key);
+                System.out.println("Текст расшифрован?");
+                System.out.println("Yes/No");
+            }
+        }
+
+
+        }
+    }
