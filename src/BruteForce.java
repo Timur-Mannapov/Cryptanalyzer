89937e82ded41b26c1a712dfdee413a4681269c4
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-05 15:55:15 +0300

    Delete BruteForce and StatisticalAnalysis

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -2,24 +2,7 @@ import java.util.Scanner;
 
 public class BruteForce {
     public void bruteForce() {
-        Scanner scanner = new Scanner(System.in);
-        CaesarCipher caesarCipher = new CaesarCipher();
-        System.out.println("Введите текст, который нужно расшифровать");
-        String string = scanner.nextLine();
-        int key = 1;
-        String decryptText = caesarCipher.decrypt(string, key);
-        System.out.println(decryptText);
-        System.out.println("Текст расшифрован?");
-        System.out.println("Да/Нет");
-        String answer = scanner.nextLine();
-        while (answer.equals("Нет")) {
-            while (!(decryptText.equals(", ")) && !(decryptText.equals(". "))) {
-                key += 1;
-                System.out.println(decryptText);
-                System.out.println("Текст расшифрован?");
-                System.out.println("Да/Нет");
-                answer = scanner.nextLine();
+
             }
-        }
     }
-}
+
--- src/StatisticalAnalysis.java
+++ src/StatisticalAnalysis.java
@@ -3,23 +3,7 @@ import java.util.Scanner;
 
 public class StatisticalAnalysis {
     public void statisticalAnalysis() {
-        Scanner scanner = new Scanner(System.in);
-        CaesarCipher caesarCipher = new CaesarCipher();
-        System.out.println("Введите текст, который нужно расшифровать");
-        String string = scanner.nextLine();
-        int key = 1;
-        String decryptText = caesarCipher.decrypt(string, key);
-        String anotherText = scanner.nextLine();
-        int i = 0;
-        int i2 = 5;
-        String fiveLetters = String.valueOf(decryptText.indexOf(i,i2));
-        while (!anotherText.contains(fiveLetters)) {
-            i += 5;
-            i2 += 5;
-            while (!(anotherText.contains(fiveLetters))) {
-                key += 1;
-                System.out.println(decryptText);
+
             }
         }
-    }
-}
+
