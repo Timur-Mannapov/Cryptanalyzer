1a539ca05d459105719a64868cb349077917e256
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-06 11:49:37 +0300

    in Class CaesarCipher changed ALPHABET

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,15 +1,17 @@
 
 public class CaesarCipher {
-    private static final String alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,””:-!? ";
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
+            ".,\":!? +-*/\\@#$%^&(){}[];'|`~=_©«»—…" + "0123456789";
 
 
     public String encrypt(String message, int key) {
         StringBuilder builder = new StringBuilder();
         for (char aChar : message.toCharArray()) {
-            int index = alphabet.indexOf(aChar);
+            int index = ALPHABET.indexOf(aChar);
             if (index >= 0) {
-                int newIndex = (index + key) % alphabet.length();
-                char charAt = (newIndex < 0) ? alphabet.charAt(newIndex + alphabet.length()) : alphabet.charAt(newIndex);
+                int newIndex = (index + key) % ALPHABET.length();
+                char charAt = ALPHABET.charAt(newIndex < 0 ? newIndex + ALPHABET.length() : newIndex);
                 builder.append(charAt);
             }
         }
