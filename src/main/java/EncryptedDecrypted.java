4cb463dafef4046f72693a3589b8bcecc9206828
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-20 16:21:43 +0300

    Not changed

--- src/main/java/EncryptedDecrypted.java
+++ src/main/java/EncryptedDecrypted.java
@@ -1,18 +1,15 @@
 import java.io.*;
 import java.nio.file.Path;
-import java.util.Scanner;
 
 public class EncryptedDecrypted {
-    /* Запросить у пользователя адрес файла, который нужно зашифровать и дешифровать,
-    запросить ключ и запросить куда записать результат
-     */
+
     public void encryptedDecrypted(boolean flag) throws IOException {
 
-        System.out.println("Введите адрес файла, который нужно зашифровать");
+        System.out.println("Введите адрес файла, который нужно зашифровать/дешифровать");
         String src = Util.readString();
         System.out.println("Введите ключ для шифрования/дешифрования");
         int key = Util.readInt();
-        Path dest = Util.buildFileName(src,"result");
+        Path dest = Util.buildFileName(src, "result.txt");
         try (BufferedReader reader = new BufferedReader(new FileReader(src));
              BufferedWriter writer = new BufferedWriter(new FileWriter(dest.toFile()))) {
             CaesarCipher caesarCipher = new CaesarCipher();
@@ -22,13 +19,11 @@ public class EncryptedDecrypted {
                 if (flag) {
                     result = caesarCipher.encrypt(string, key);
                     writer.write(result);
-                } else if (!flag){
+                } else {
                     result = caesarCipher.decrypt(string, key);
                     writer.write(result);
                 }
             }
-
         }
-
     }
 }
