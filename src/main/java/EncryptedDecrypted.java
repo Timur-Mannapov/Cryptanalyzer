03e1c63e5ffd3197912a38f140206f1f15f0247c
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-20 20:24:21 +0300

    refactoring class EncryptedDecrypted

--- src/main/java/EncryptedDecrypted.java
+++ src/main/java/EncryptedDecrypted.java
@@ -1,28 +1,25 @@
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class EncryptedDecrypted {
 
+    private final CaesarCipher caesarCipher = new CaesarCipher();
+
     public void encryptedDecrypted(boolean flag) throws IOException {
 
-        System.out.println("Введите адрес файла, который нужно зашифровать/дешифровать");
+        Util.writeMessage("Введите адрес файла, который нужно " + (flag ? "зашифровать" : "дешифровать"));
         String src = Util.readString();
-        System.out.println("Введите ключ для шифрования/дешифрования");
+        Util.writeMessage("Введите ключ");
         int key = Util.readInt();
-        Path dest = Util.buildFileName(src, "result.txt");
-        try (BufferedReader reader = new BufferedReader(new FileReader(src));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(dest.toFile()))) {
-            CaesarCipher caesarCipher = new CaesarCipher();
+        Path dest = Util.buildFileName(src, flag ? "_encrypted" : "_decrypted");
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(src));
+             BufferedWriter writer = Files.newBufferedWriter(dest)) {
             while (reader.ready()) {
                 String string = reader.readLine();
-                String result;
-                if (flag) {
-                    result = caesarCipher.encrypt(string, key);
-                    writer.write(result);
-                } else {
-                    result = caesarCipher.decrypt(string, key);
-                    writer.write(result);
-                }
+                String result = flag ? caesarCipher.encrypt(string, key) : caesarCipher.decrypt(string, key);
+                writer.write(result);
+                writer.newLine();
             }
         }
     }
