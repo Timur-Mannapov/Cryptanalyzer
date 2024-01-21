9ac6feb7b5e417a348116913cefa5d0ea9a72cde
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-21 17:06:57 +0300

    create Class StatisticalAnalysis

--- src/main/java/StatisticalAnalysis.java
+++ src/main/java/StatisticalAnalysis.java
@@ -1,9 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Path;
+import java.io.*;
 
 
 public class StatisticalAnalysis {
-    public void statisticalAnalysis() {
-
+    public void statisticalAnalysis() throws IOException {
+        System.out.println("Введите адрес файла, который нужно дешифровать");
+        String src = Util.readString();
+        Path dest = Util.buildFileName(src, "result.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(src));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(dest.toFile()))) {
+            CaesarCipher caesarCipher = new CaesarCipher();
+            while (reader.ready()) {
             }
         }
-
+    }
+}
