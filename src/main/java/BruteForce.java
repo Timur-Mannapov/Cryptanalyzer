500beae199b3fbcf43585bec89f443333052afd9
Timur Mannapov <bla.blacarr@yandex.ru>
2024-02-03 20:46:12 +0300

    Изменил класс BruteForce, финальный вариант

--- src/main/java/BruteForce.java
+++ src/main/java/BruteForce.java
@@ -1,6 +1,5 @@
 import lombok.SneakyThrows;
 
-import java.awt.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,20 +16,64 @@ public class BruteForce {
         Util.writeMessage("Введите адрес файла, к которому необходимо подобрать ключ");
         String src = Util.readString();
         Path dest = Util.buildFileName(src, "_bruteForce");
+        String content = Files.readString(Path.of(src));
+        for (int i = 0; i < caesarCipher.alphabetLength(); i++) {
+            String decrypt = caesarCipher.decrypt(content, i);
+            if (isValidateText(decrypt)) {
+                Files.writeString(dest,decrypt);
+                Util.writeMessage("Содержимое файла расшифровано, ключ расшифровки равен " + i);
+                break;
+            }
+        }
+    }
+
+    @SneakyThrows
+    public void bruteForce1() {
+        Util.writeMessage("Введите адрес файла, к которому необходимо подобрать ключ");
+        String src = Util.readString();
+        Path dest = Util.buildFileName(src, "_bruteForce");
+        List<String> list = Files.readAllLines(Path.of(src));
+        String string = String.join(System.lineSeparator(), list);
+
+        for (int i = 0; i < caesarCipher.alphabetLength(); i++) {
+            String decrypt = caesarCipher.decrypt(string, i);
+            if (isValidateText(decrypt)) {
+                StringBuilder builder = new StringBuilder();
+                for (String str : list) {
+                    builder.append(caesarCipher.decrypt(str, i)).append(System.lineSeparator());
+                }
+                Files.write(dest, builder.toString().getBytes());
+                Util.writeMessage("Содержимое файла расшифровано, ключ расшифровки равен " + i);
+                break;
+            }
+        }
+    }
+
+
+    @SneakyThrows
+
+    public void bruteForce2() {
+        Util.writeMessage("Введите адрес файла, к которому необходимо подобрать ключ");
+        String src = Util.readString();
+        Path dest = Util.buildFileName(src, "_bruteForce");
         try (BufferedReader reader = Files.newBufferedReader(Path.of(src));
              BufferedWriter writer = Files.newBufferedWriter(dest)) {
             List<String> list = new ArrayList<>();
+            StringBuilder builder = new StringBuilder();
             while (reader.ready()) {
                 String string = reader.readLine();
                 list.add(string);
-                list.add("\n");
+                builder.append(string);
             }
 
             for (int i = 0; i < caesarCipher.alphabetLength(); i++) {
-                String decrypt = caesarCipher.decrypt(list.toString(), i);
+                String decrypt = caesarCipher.decrypt(builder.toString(), i);
                 if (isValidateText(decrypt)) {
-                    writer.write(decrypt);
-                    writer.newLine();
+                    for (String string : list) {
+                        String str = caesarCipher.decrypt(string, i);
+                        writer.write(str);
+                        writer.newLine();
+                    }
                     Util.writeMessage("Содержимое файла расшифровано, ключ расшифровки равен " + i);
                     break;
                 }
