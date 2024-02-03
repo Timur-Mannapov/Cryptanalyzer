3b019dc9d7843f12ad03a44c137521dffb83b62f
Timur Mannapov <bla.blacarr@yandex.ru>
2024-02-03 20:50:23 +0300

    Изменен класс EncryptedDecrypted, добавлена новая реализация метода encryptedDecrypted

--- src/main/java/EncryptedDecrypted.java
+++ src/main/java/EncryptedDecrypted.java
@@ -1,3 +1,5 @@
+import lombok.SneakyThrows;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,7 +8,18 @@ public class EncryptedDecrypted {
 
     private final CaesarCipher caesarCipher = new CaesarCipher();
 
-    public void encryptedDecrypted(boolean flag) throws IOException {
+    @SneakyThrows
+    public void encryptedDecrypted(boolean flag) {
+        Util.writeMessage("Введите адрес файла, который нужно " + (flag ? "зашифровать" : "дешифровать"));
+        String src = Util.readString();
+        Util.writeMessage("Введите ключ");
+        int key = Util.readInt();
+        Path dest = Util.buildFileName(src, flag ? "_encrypted" : "_decrypted");
+        String content = Files.readString(Path.of(src));
+        Files.writeString(dest, flag ? caesarCipher.encrypt(content, key) : caesarCipher.decrypt(content, key));
+    }
+
+    public void encryptedDecrypted1(boolean flag) throws IOException {
 
         Util.writeMessage("Введите адрес файла, который нужно " + (flag ? "зашифровать" : "дешифровать"));
         String src = Util.readString();
